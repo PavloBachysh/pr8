7d568b4ae1846806cf73a471b48118c86234c6e6
PavloBachysh <happytouni@gmail.com>
2025-11-05 10:43:31 +0100

    my work(Bachysh)

--- prac8.py
+++ prac8.py
@@ -6,23 +6,9 @@ def MakePath(yourFileName):
     return os.path.join(base_dir, yourFileName)
 
 
-def NewStudent(number, fileName):
-    if (number <= 0):
-        print("Невірно вказаний номер студента")
-        return
-    fileName.write(f"Студент номер {number}\n")
-    secName = str(input("Введіть ваше призвіще: "))
-    fileName.write(f"Моє прізвище - {secName}\n")
-
-    if (number > 1):
-        answer = str(input("Введіть вашу відповідь на запитання: "))
-        fileName.write(f"Моя вівдповідь - {answer}\n")
-    
-    question = str(input("Введіть ваше запитання: "))
-    fileName.write(f"Моє запитання - {question}\n")
-    fileName.write("----------------------------------------\n")
-
-
 myFile = open(MakePath(myFileName), 'w')
-NewStudent(1, myFile)
+myFile.write("Студент номер 1\n")
+myFile.write("Моє прізвище - Бачиш\n")
+myFile.write("Моє запитання - Що таке словник в Python?\n")
+myFile.write("----------------------------------------\n")
 myFile.close()
