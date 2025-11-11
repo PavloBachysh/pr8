968ceb3d4768c487b68c73d1e8fb33ffde47ad16
Bogdan <sagaydak.bogdan@gmail.com>
2025-11-11 14:54:40 +0200

    2.1

--- prac8.py
+++ prac8.py
@@ -5,21 +5,30 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 def MakePath(yourFileName):
     return os.path.join(base_dir, yourFileName)
 
-myFile = open(MakePath(myFileName), 'w')
-myFile.write("Студент номер 1\n")
-myFile.write("Моє прізвище - Бачиш\n")
-myFile.write("Моє запитання - Що таке словник в Python?\n")
-myFile.write("----------------------------------------\n")
-myFile.close()
+try:
+    myFile = open(MakePath(myFileName), 'w')
+    myFile.write("Студент номер 1\n")
+    myFile.write("Моє прізвище - Бачиш\n")
+    myFile.write("Моє запитання - Що таке словник в Python?\n")
+    myFile.write("----------------------------------------\n")
+    myFile.close()
+except:
+    print("Error 1")
 
-with open(MakePath(myFileName), 'a') as file:
-    file.write("Студент номер 2\n")
-    file.write("Моє прізвище - Сагайдак\n")
-    file.write("Моя відповідь - структура даних, що зберігає дані у вигляді ключ-значення\n")
-    file.write("Моє запитання - Які основні типи даних в Python?\n")
-    file.write("----------------------------------------\n")
+try:
+    with open(MakePath(myFileName), 'a') as file:
+        file.write("Студент номер 2\n")
+        file.write("Моє прізвище - Сагайдак\n")
+        file.write("Моя відповідь - структура даних, що зберігає дані у вигляді ключ-значення\n")
+        file.write("Моє запитання - Які основні типи даних в Python?\n")
+        file.write("----------------------------------------\n")
+except:
+    print("Error 2")
 
-with open(MakePath(myFileName), 'r') as file:
-    file.seek(0)
-    text = file.read()
-    print(text)
+try:
+    with open(MakePath(myFileName), 'r') as file:
+        file.seek(0)
+        text = file.read()
+        print(text)
+except:
+    print("Error 3")
